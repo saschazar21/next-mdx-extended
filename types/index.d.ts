f8db28f2b435fd2c6b37745ef05c5eadd470fbc1
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2019-12-23 12:00:59 +0100

    fix(gitignore): added generated types directory to gitignore

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { MDXExtendedLoaderOptions } from '@saschazar/mdx-extended-loader';
-import { Configuration } from 'webpack';
-import { NextConfig } from "./types/nextConfig";
-import { NextOptions } from "./types/nextOptions";
-declare const _default: (pluginOptions: MDXExtendedLoaderOptions) => (nextConfig: NextConfig) => NextConfig & {
-    webpack(config: Configuration, options: NextOptions): any;
-};
-export default _default;
