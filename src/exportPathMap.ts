705384f8aaa920836d9138089391f7bd47efc37d
Sascha Zarhuber <sascha.zarhuber@gmail.com>
2020-01-05 10:25:17 +0100

    fix(exportPathMap): improved error output

--- src/exportPathMap.ts
+++ src/exportPathMap.ts
@@ -69,7 +69,7 @@ export default async function exportPathMap(
         url = parseFormat(format, meta); // replace the placeholders in the format with the actual values
         postsMeta.push(Object.assign({}, meta, { url: join('/', url) }));
       } catch (e) {
-        console.error(e);
+        console.error(post, ':\n', e.message || e);
       }
 
       // add the rewritten path to the PathMap
